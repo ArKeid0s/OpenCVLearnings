c5b1be7d890333d46d80ffd7b1af502c6f70e618
arkeid0s <matproz.gaming@gmail.com>
2023-05-07 15:46:01 +0200

    Contours detection

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 import os
 import cv2
-import numpy as np
 
-img = cv2.imread(os.path.join("data", "whiteboard.png"))
+img = cv2.imread(os.path.join("data", "birds.jpg"))
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Lines
-cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 3)
+ret, thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
 
-# Rectangles
-cv2.rectangle(img, (200, 350), (450, 600), (0, 0, 255), -1)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# Circles
-cv2.circle(img, (800, 200), 75, (255, 0, 0), 10)
-
-# Text
-cv2.putText(img, "OpenCV", (600, 450), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 0), 2)
+for contour in contours:
+    if cv2.contourArea(contour) > 200:
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                cv2.drawContours(img, contour, -1, (0, 0, 255), 2)
 
 cv2.imshow("img", img)
+cv2.imshow("thresh", thresh)
 cv2.waitKey(0)
