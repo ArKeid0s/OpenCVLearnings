235e990b61973a280cb14c0466c633bc207d46ae
arkeid0s <matproz.gaming@gmail.com>
2023-05-07 12:19:08 +0200

    Blur filters and use case to remove image noise

--- src/main.py
+++ src/main.py
@@ -1,17 +1,25 @@
 import os
 import cv2
 
-img = cv2.imread(os.path.join('data', 'man_portal.jpg'))
+img = cv2.imread(os.path.join("data", "freelancer.jpeg"))
+img_noise = cv2.imread(os.path.join("data", "cow-salt-peper.png"))
 
-# Convert BGR to RGB
-# img_modified = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# Blur filters
+# k_size = 11  # larger k_size means more blur
+# img_blur = cv2.blur(img, (k_size, k_size))
+# img_blur = cv2.GaussianBlur(img, (k_size, k_size), 5)
+# img_blur = cv2.medianBlur(img, k_size)
 
-# Convert BGR to GRAY
-# img_modified = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Use blur to remove noise
+k_size = 7
+img_noise_blur = cv2.blur(img_noise, (k_size, k_size))
+img_noise_blur_gaussian = cv2.GaussianBlur(img_noise, (k_size, k_size), 5)
+img_noise_blur_median = cv2.medianBlur(img_noise, k_size) # the noise is removed better
 
-# Convert BGR to HSV
-img_modified = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-cv2.imshow('img', img)
-cv2.imshow('img_modified', img_modified)
+# cv2.imshow("img", img)
+# cv2.imshow("img_blur", img_blur)
+cv2.imshow("img_noise", img_noise)
+cv2.imshow("img_noise_blur", img_noise_blur)
+cv2.imshow("img_noise_blur_gaussian", img_noise_blur_gaussian)
+cv2.imshow("img_noise_blur_median", img_noise_blur_median)
 cv2.waitKey(0)
